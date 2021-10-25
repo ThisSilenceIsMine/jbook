048dcae604eceac581b01560e16694574482fb47
ThisSilenceIsMine <aurakel542@gmail.com>
2021-10-26 02:53:21 +0300

    Loading nested packages

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -5,40 +5,45 @@ export const unpkgPathPlugin = () => {
     return {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
-            build.onResolve({ filter: /.*/ }, async (args: any) => {
-                console.log('onResolve', args);
-                if (args.path === 'index.js') {
-                    return { path: args.path, namespace: 'a' };
-                } 
-                if(args.path.includes('./') || args.path.includes('../')) {
+            build.onResolve(
+                { filter: /.*/ },
+                async (args: esbuild.OnResolveArgs) => {
+                    console.log('onResolve', args);
+                    if (args.path === 'index.js') {
+                        return { path: args.path, namespace: 'a' };
+                    }
+                    if (args.path.includes('./') || args.path.includes('../')) {
+                        return {
+                            namespace: 'a',
+                            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
+                        };
+                    }
                     return {
                         namespace: 'a',
-                        path: new URL(args.path, args.importer + '/').href
-                    }
+                        path: `https://unpkg.com/${args.path}`,
+                    };
                 }
-                return {
-                    namespace: 'a',
-                    path: `https://unpkg.com/${args.path}`
-                }
-            });
+            );
 
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
+            build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
                 console.log('onLoad', args);
 
                 if (args.path === 'index.js') {
                     return {
                         loader: 'jsx',
                         contents: `
-                const message = require('medium-test-pkg');
+                const message = require('nested-test-pkg');
                 console.log(message);
             `,
                     };
                 }
 
-                const { data } = await axios.get(args.path);
+                const { data, request } = await axios.get(args.path);
+                console.log(request);
                 console.log(data);
                 return {
                     loader: 'jsx',
+                    resolveDir: new URL('./', request.responseURL).pathname,
                     contents: data,
                 };
             });
