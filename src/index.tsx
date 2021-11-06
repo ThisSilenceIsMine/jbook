9a278e69bc6c526d5898bdbe9a86879f87abb32d
ThisSilenceIsMine <aurakel542@gmail.com>
2021-11-06 22:05:20 +0200

    Simple code execution in IFrame.

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import ReactDom from 'react-dom';
 import * as esbuild from 'esbuild-wasm';
 import { useEffect } from 'react';
@@ -6,52 +6,70 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
-    const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const iframe = useRef<any>();
 
-    const startService = async () => {
-        try {
-            await esbuild.initialize({
-                worker: true,
-                wasmURL: 'https://unpkg.com/esbuild-wasm@0.13.8/esbuild.wasm',
-            });
-            console.log(esbuild);
-        } catch (err) {
-            console.error(err);
-        }
-    };
+  const startService = async () => {
+    try {
+      await esbuild.initialize({
+        worker: true,
+        wasmURL: 'https://unpkg.com/esbuild-wasm@0.13.8/esbuild.wasm',
+      });
+      console.log(esbuild);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    useEffect(() => {
-        startService();
-    }, []);
+  useEffect(() => {
+    startService();
+  }, []);
 
-    const onClick = async () => {
-        const result = await esbuild.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-            define: {
-                'process.env.NODE_ENV': '"production"',
-                global: 'window',
-            },
-        });
+  const onClick = async () => {
+    const result = await esbuild.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
 
-        setCode(result.outputFiles[0].text);
-    };
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+  };
 
-    return (
-        <>
-            <textarea
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            ></textarea>
-            <div>
-                <button onClick={onClick}>Submit</button>
-            </div>
-            <pre>{code}</pre>
-        </>
-    );
+  const html = `
+      <html>
+        <head>
+        </head>
+        <body>
+          <div id="root"></div>
+          <script>
+            window.addEventListener('message', (event) => {
+              eval(event.data);
+            }, false);
+          </script>
+        </body>
+      </html>
+    `;
+
+  return (
+    <>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+      <iframe ref={iframe} title="playground" sandbox="allow-scripts" srcDoc={html} />
+    </>
+  );
 };
 
 ReactDom.render(<App />, document.querySelector('#root'));
