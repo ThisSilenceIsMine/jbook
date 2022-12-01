358cd8f1cc192a7ad12c7c3fbc423762606e82ac
ThisSilenceIsMine <aurakel542@gmail.com>
2022-12-02 00:30:03 +0200

    First e2e test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,7 @@
 describe('Code block flow', () => {
   it('creates new code block', () => {
-    cy.visit('https://example.cypress.io');
+    cy.visit('localhost:3000');
+    cy.contains('New Code Block').click();
+    cy.get('.editor-wrapper').should('be.visible');
   });
 });
