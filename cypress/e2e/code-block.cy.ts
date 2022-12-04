40cd8124334be857cea4a7459bd1ff88af4d2300
ThisSilenceIsMine <aurakel542@gmail.com>
2022-12-04 16:43:59 +0200

    codeblock execution test

--- cypress/e2e/code-block.cy.ts
+++ cypress/e2e/code-block.cy.ts
@@ -8,22 +8,29 @@ describe('Code block flow', () => {
   it('runs correct code', () => {
     cy.visit('localhost:3000');
     cy.contains('New Code Block').click();
+    const testString = 'Some completely random test string';
 
     const code = `
       import React from 'react';
       import ReactDOM from 'react-dom';
 
-      const App = () => {
-        return <div>Hello, world!</div>
-      }
+      const App = () =>
+         <div>${testString}</div>
 
-      ReactDOM.render(document.querySelector('#root', <App/>));
+      ReactDOM.render(<App/>, document.querySelector('#root'));
     `;
 
     cy.get('.view-lines')
       .click()
       // change subject to currently focused element
       .focused()
-      .type(code);
+      .type(code, { delay: 1 });
+
+    cy.wait(4000);
+
+    cy.get('iframe[data-cy="preview"]')
+      .iframe()
+      .contains(testString)
+      .should('exist');
   });
 });
